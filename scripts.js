ad1e6a94546e1de1ea14825e56ca7a10ab7e7d9e
arndislilja <ath216@hi.is>
2018-11-12 20:07:51 +0000

    byrjaði aðeins á verkefninu .js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
 // const API_URL = '/example.json?domain=';
 const API_URL = 'https://apis.is/isnic?domain=';
 
+document.addEventListener
+('DOMContentLoaded', function () {
+  const domain = document.querySelector('.domains');
+
+  program.init(domains);
+});
+
 /**
  * Leit að lénum á Íslandi gegnum apis.is
  */
 const program = (() => {
-  function init() {
+  let domains;
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    console.log(e);
+  }
+
+  function init(_domains) {
+    domains = _domains;
+    
+    const form = domains.querySelector('form');
+    form.addEventListener('Submit', onSubmit);
 
   }
 
